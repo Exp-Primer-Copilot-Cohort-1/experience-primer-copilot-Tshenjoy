a1e745b261b9815e1a0c56faee471960d89f9a22
Tshenjoy <74413435+Tshenjoy@users.noreply.github.com>
2024-03-22 06:43:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,44 +1,20 @@
 // create web server
-// create a web server
+// create express server
+import express from 'express';
+const app = express();
+// create server
 import { createServer } from 'http';
-import { parse } from 'url';
-import { readFile } from 'fs';
-import { parse as _parse } from 'querystring';
-// create a web server
-createServer(function (req, res) {
-  var query = parse(req.url, true).query;
-  var pathname = parse(req.url).pathname;
-  var filename = "." + pathname;
-  if (pathname === '/comments') {
-    if (req.method === 'POST') {
-      var body = '';
-      req.on('data', function (data) {
-        body += data;
-      });
-      req.on('end', function () {
-        var POST = _parse(body);
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('You sent the comment: ' + POST.comment);
-        res.end();
-      });
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write('<form action="/comments" method="post">');
-      res.write('<input type="text" name="comment">');
-      res.write('<input type="submit">');
-      res.write('</form>');
-      return res.end();
-    }
-  } else {
-    readFile(filename, function (err, data) {
-      if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        return res.end("404 Not Found");
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end();
-    });
-  }
-}).listen(8080);
-console.log('Server running at http://');
+const server = createServer(app);
+// create socket.io
+import socketIo from 'socket.io';
+const io = socketIo(server);
+// create body parser
+import { json } from 'body-parser';
+app.use(json());
+// create mongoose
+import { Schema as _Schema } from 'mongoose';
+// create mongoose schema
+const Schema = _Schema;
+const commentSchema = new Schema({
+    // code for commentSchema goes here
+});
